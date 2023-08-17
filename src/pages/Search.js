b3c5833d83336ae50255871ed6e7ccd6c74d59d3
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2023-08-17 19:56:54 +0300

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -15,76 +15,65 @@ const Search = () => {
     setContent(results);
   };
 
+  const triggerSearch = () => {
+    fetchSearch();
+  };
+
   useEffect(() => {
     fetchSearch();
-  }, []);
+  }, [page]); // Only run fetchSearch when the page changes
 
-  const Search = () => {
-    fetchSearch();
-  };
-
-  const Trigger = (e) => {
-    setSearchText(e.target.value);
-  };
   return (
-    <>
-      <div className="container">
-        <div className="row pt-3 mb-5 pb-5">
-          <div className="col-12 pt-5 pb-3 mt-5 d-flex justify-content-center align-items-center">
-            <input
-              type="text"
-              placeholder="search..."
-              onChange={Trigger}
-              className="form-control-lg col-6 search bg-dark text-white border border-0"
-            />
-            <button
-              className="btn btn-primary text-white mx-2 col-md-1 col-sm-2 py-2"
-              onClick={Search}
-            >
-              <i className="fas fa-search"></i>
-            </button>
-          </div>
-          {content &&
-            content.map((Val) => {
-              const {
-                name,
-                title,
-                poster_path,
-                first_air_date,
-                release_date,
-                media_type,
-                id,
-              } = Val;
-              return (
-                <>
-                  <div className="col-md-3 col-sm-4 py-3" id="card" key={id}>
-                    <div className="card bg-dark" key={id}>
-                      <img
-                        src={
-                          poster_path
-                            ? `${img_300}/${poster_path}`
-                            : unavailable
-                        }
-                        className="card-img-top pt-3 pb-0 px-3"
-                      />
-                      <div className="card-body">
-                        <h5 className="card-title text-center fs-5">
-                          {title || name}
-                        </h5>
-                        <div className="d-flex fs-6 align-items-center justify-content-evenly movie">
-                          <div>{media_type === "tv" ? "TV" : "Movie"}</div>
-                          <div>{first_air_date || release_date}</div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          {page > 1 && <Pagination page={page} setPage={setPage} />}
+    <div className="container">
+      <div className="row pt-3 mb-5 pb-5">
+        <div className="col-12 pt-5 pb-3 mt-5 d-flex justify-content-center align-items-center">
+          <input
+            type="text"
+            placeholder="search..."
+            onChange={(e) => setSearchText(e.target.value)}
+            className="form-control-lg col-6 search bg-dark text-white border border-0"
+          />
+          <button
+            className="btn btn-primary text-white mx-2 col-md-1 col-sm-2 py-2"
+            onClick={triggerSearch}
+          >
+            <i className="fas fa-search"></i>
+          </button>
         </div>
+        {content.map((Val) => {
+          const {
+            name,
+            title,
+            poster_path,
+            first_air_date,
+            release_date,
+            media_type,
+            id,
+          } = Val;
+          return (
+            <div className="col-md-3 col-sm-4 py-3" id="card" key={id}>
+              <div className="card bg-dark">
+                <img
+                  src={poster_path ? `${img_300}/${poster_path}` : unavailable}
+                  className="card-img-top pt-3 pb-0 px-3"
+                  alt={title || name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-center fs-5">
+                    {title || name}
+                  </h5>
+                  <div className="d-flex fs-6 align-items-center justify-content-evenly movie">
+                    <div>{media_type === "tv" ? "TV" : "Movie"}</div>
+                    <div>{first_air_date || release_date}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        {page > 1 && <Pagination page={page} setPage={setPage} />}
       </div>
-    </>
+    </div>
   );
 };
 
