7203d214da74d987b8d4f460f8b95a7af3532b26
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2023-08-17 18:38:53 +0300

    Update Routes.jsx

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,31 +1,30 @@
+// src/routes/Routes.jsx
+
 import React from 'react';
-
 import { Route, Switch } from 'react-router-dom';
-
 import Home from '../pages/Home';
 import Catalog from '../pages/Catalog';
 import Detail from '../pages/Detail';
 import MovieWatch from '../pages/MovieWatch';
+import Search from '../pages/Search'; // Import the Search component
 
 const Routes = () => {
     return (
         <Switch>
             <Route path='/:category/search/:keyword'>
-                <Catalog/>
+                <Search /> {/* Use the Search component for search results */}
             </Route>
             <Route path='/:category/:id/watch'>
-                <MovieWatch/>
+                <MovieWatch />
             </Route>
             <Route path='/:category/:id'>
-                <Detail/>
+                <Detail />
             </Route>
             <Route path='/:category'>
-                <Catalog/>
+                <Catalog />
             </Route>
-           
-
             <Route path='/' exact>
-                <Home/>
+                <Home />
             </Route>
         </Switch>
     );
